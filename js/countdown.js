39068fb987c2b068fa63d4c19774898413d581ac
coinfactory768 <coinfactory768@gmail.com>
2021-11-18 15:46:15 +0100

    updated countdown

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("nov 30, 2021 15:37:25").getTime();
+var countDownDate = new Date("nov 22, 2021 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
